0afcc47dc10fe9255c7034268c6ef8744fa04b24
Nathan Tarbert <nate4t@hotmail.comm>
2021-06-22 08:20:17 -0700

    id route and styling added

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -1,13 +1,14 @@
 import Layout from '../../components/SearchBar/Layout';
 import styles from './Coin.module.css';
 import { useRouter } from "next/router";
+import Coins from '../../components/Coins';
 
 const Coin = ({ coin }) => {
     const router = useRouter();
 
   return (
     <Layout>
-        <button style={{color: 'white', fontSize: '50px', borderBlockColor: 'red', backgroundColor: 'blue'}} type="button" onClick={() => router.push('/')}>
+        <button className={styles.coin_button} type="button" onClick={() => router.push('/')}>
             Home
         </button>
       <div className={styles.coin_page}>
@@ -21,6 +22,26 @@ const Coin = ({ coin }) => {
           <p className={styles.coin_ticker}>{coin.symbol.toUpperCase()}</p>
           <p className={styles.coin_current}>
             ${coin.market_data.current_price.usd}
+          </p>
+
+          {coin.market_data.price_change_24h < 0 ? (
+                <p className={(styles.coin_price_change_24h, styles.red)}>
+                  ${coin.market_data.price_change_24h.toFixed(2)}
+                </p>
+              ) : (
+                <p className={(styles.coin_price_change_24h, styles.green)}>
+                  ${coin.market_data.price_change_24h.toFixed(2)}
+                </p>
+              )}
+
+          {/* <p className={styles.coin_price_change_24h}>
+            {coin.market_data.price_change_24h.toFixed(3)} %
+          </p> */}
+          <p className={styles.coin_high_24h}>
+              24hr High ${coin.market_data.high_24h.usd}
+          </p>
+          <p className={styles.coin_low_24h}>
+              24hr Low ${coin.market_data.low_24h.usd}
           </p>
         </div>
       </div>
