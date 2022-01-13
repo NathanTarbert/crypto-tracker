b1dfdcce33d3e85f215bee226a5dbf343abeb262
nate4t 5 <66887028+NathanTarbert@users.noreply.github.com>
2022-01-13 11:34:24 -0800

    added styling to navbar

--- components/Nav.js
+++ components/Nav.js
@@ -4,43 +4,38 @@ import Watchlist from '../pages/watchlist';
 import { Container } from 'react-bootstrap';
 
 function Nav() {
-    return (
-        <nav className={navStyles.nav}>
-            <div className="octo">
-                <Link href='/' passHref>
-                    <a>
-                        <img src='/rocket1.png' alt="crypto-tracker-logo" />
-                    </a>
-                </Link>
-            </div>
-           <ul>
-                {/* <li>
+	return (
+		<nav className={navStyles.nav}>
+			<div className="octo">
+				<Link href="/" passHref>
+					<a>
+						<img src="/rocket1.png" alt="crypto-tracker-logo" />
+					</a>
+				</Link>
+			</div>
+			<div>
+				<h1 style={navStyles.h1}>Crypto Tracker</h1>
+			</div>
+
+			<ul>
+				{/* <li>
                    <Link href='/'>Home</Link> 
                 </li> */}
-                
-                <li>
-                   <Link href='/about'>About</Link> 
-                </li> 
-                 <li>
-                   <Link href='/watchlist'>Watchlist</Link> 
-                </li>
-                
-            </ul> 
-            <ul >
-                <li>
-                    <Link href='/docs'>Docs</Link>
-                </li>
-                
-            </ul>
-            <ul >
-                <li>
-                    <h1 style={navStyles.h1}>Crypto Tracker</h1>
-                </li>
-                
-            </ul>
-            
-        </nav>
-    )
-}   
 
-export default Nav
+				<li>
+					<Link href="/about">About</Link>
+				</li>
+				<li>
+					<Link href="/watchlist">Watchlist</Link>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<Link href="/docs">Docs</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,24 @@
 import Layout from '../components/Layout';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function about() {
 	return (
 		<div className="container">
 			<h1 className="text-center">What is Crypto Currency?</h1>
 			<br />
-			<p className="md:container md:mx-auto">
-				At its core, cryptocurrency is typically decentralized digital money designed to be used over the
-				internet. Bitcoin, which launched in 2008, was the first cryptocurrency, and it remains by far the
-				biggest, most influential, and best-known. In the decade since, Bitcoin and other cryptocurrencies like
-				Ethereum have grown as digital alternatives to money issued by governments.
-			</p>
+			<Container>
+				<Row>
+					<Col style={{ backgroundColor: 'white', paddig: '1rem' }}>
+						<p style={{ color: 'black' }}>
+							At its core, cryptocurrency is typically decentralized digital money designed to be used
+							over the internet. Bitcoin, which launched in 2008, was the first cryptocurrency, and it
+							remains by far the biggest, most influential, and best-known. In the decade since, Bitcoin
+							and other cryptocurrencies like Ethereum have grown as digital alternatives to money issued
+							by governments.
+						</p>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 }
